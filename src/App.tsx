535182b09557d2b5c6724f8779f723e8e4ed3059
Zishan Ansari <kinnngg786@gmail.com>
2021-01-02 18:45:01 +0530

    Add What Xinecraft Say

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Highlighter from "react-highlight-words";
+import swal from '@sweetalert/with-react'
 
 interface DataType {
   rows: Array<any>;
@@ -9,7 +11,8 @@ interface MyState {
   searchTerm: string;
   quranResult: DataType | null;
   bibleResult: DataType | null;
-};
+  isLoading: boolean;
+}
 
 class App extends React.Component<{}, MyState> {
   constructor(props: any) {
@@ -18,6 +21,7 @@ class App extends React.Component<{}, MyState> {
       searchTerm: "",
       quranResult: null,
       bibleResult: null,
+      isLoading: false,
     };
   }
 
@@ -25,23 +29,23 @@ class App extends React.Component<{}, MyState> {
     this.setState({ searchTerm: e.target.value });
   }
 
-  handleEnterKeyPress (e: any) {
-    if(e.key === 'Enter'){
-      this.searchForTerm()
+  handleEnterKeyPress(e: any) {
+    if (e.key === "Enter") {
+      this.searchForTerm();
     }
   }
 
   async searchForTerm() {
-    if (this.state.searchTerm === "") {
+    if (this.state.searchTerm === "" || this.state.isLoading) {
       return;
     }
+    this.setState({ isLoading: true });
     const parsedBibleData = await this.searchBibleAndParse();
     const parsedQuranData = await this.searchQuranAndParse();
 
     this.setState({ bibleResult: parsedBibleData });
     this.setState({ quranResult: parsedQuranData });
-    console.log(parsedBibleData);
-    console.log(parsedQuranData);
+    this.setState({ isLoading: false });
   }
 
   async searchBibleAndParse() {
@@ -72,27 +76,105 @@ class App extends React.Component<{}, MyState> {
   }
 
   askXinecraft() {
-    alert("Just STFU! and visit my github.")
-    // window.location.href = 'http://github.com/xinecraft'
+    swal({
+     buttons: {
+       ok: 'STFU',
+       cancel: "Close",
+     },
+     content: (
+      <div>
+      <h2 className="font-bold text-xl text-light-blue-500">This World is a lie! Please visit my profile.</h2>
+      <p className="text-gray-600 italic mt-2 mb-2">
+        No No No! This is not a Request
+      </p>
+      <img src="https://i.ytimg.com/vi/LaiN63o_BxA/maxresdefault.jpg" alt=""/>
+    </div>
+    )
+    })
+
+    setTimeout(() => {
+      window.location.href = 'http://github.com/xinecraft'
+    }, 10000)
   }
 
   render() {
-    let bibleResult = null
+    let bibleResult = null;
+    const splitSearchTerm = [this.state.searchTerm, ...this.state.searchTerm.split(' ')]
     if (this.state.bibleResult && this.state.bibleResult.rows) {
       bibleResult = this.state.bibleResult.rows.map((result: any) => (
-        <li className="mb-3" key={result.id}>{result.text.toString()}</li>
+        <li key={result.id}>
+          <Highlighter
+              highlightClassName="bg-blue-200"
+              searchWords={splitSearchTerm}
+              autoEscape={true}
+              textToHighlight={result.text.toString()}
+            />
+        </li>
       ));
     }
 
-    let quranResult = null
+    let quranResult = null;
     if (this.state.quranResult && this.state.quranResult.rows) {
-      quranResult = this.state.quranResult.rows.map((result: any, index: any) => (
-        <li className="mb-3" key={index}>{result.text.toString()}</li>
-      ));
+      quranResult = this.state.quranResult.rows.map(
+        (result: any, index: any) => (
+          <li key={index}>
+            <Highlighter
+              highlightClassName="bg-green-200"
+              searchWords={splitSearchTerm}
+              autoEscape={true}
+              textToHighlight={result.text.toString()}
+            />
+          </li>
+        )
+      );
+    }
+
+    let loadingBar = null;
+    if (this.state.isLoading) {
+      loadingBar = (
+        <div>
+          <div className="p-4 w-full mx-auto">
+            <div className="animate-pulse flex space-x-4">
+              <div className="flex-1 space-y-4 py-1">
+                <div className="h-4 bg-blue-gray-300 rounded w-3/4"></div>
+                <div className="space-y-2">
+                  <div className="h-4 bg-blue-gray-300 rounded"></div>
+                  <div className="h-4 bg-blue-gray-300 rounded w-5/6"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-4 w-full mx-auto">
+            <div className="animate-pulse flex space-x-4">
+              <div className="flex-1 space-y-4 py-1">
+                <div className="h-4 bg-blue-gray-300 rounded w-3/4"></div>
+                <div className="space-y-2">
+                  <div className="h-4 bg-blue-gray-300 rounded"></div>
+                  <div className="h-4 bg-blue-gray-300 rounded w-5/6"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
     }
 
     return (
       <div>
+        <div
+          className="overflow-hidden text-center leading-normal"
+          role="alert"
+        >
+          <p className="px-4 py-3 font-bold text-purple-100 bg-purple-800">
+            Under Development! Looking for: Geeta & Guru Granth Sahib API
+          </p>
+          <p className="px-4 py-3 text-purple-700 bg-purple-100 ">
+            Looking for adding more scriptures. If you know any Scripture API that 
+            provide data based on keyword search, please let me know via{" "}
+            <a className="underline" href="https://github.com/xinecraft/whatgodsay">Github</a> or <a className="underline" href="mailto:xinecraft1337@gmail.com">Email</a>.
+          </p>
+        </div>
+
         <div className="flex flex-col items-center">
           <h1 className="text-8xl h-full p-10 font-bhs">
             <span className="text-blue-500">W</span>
@@ -105,7 +187,7 @@ class App extends React.Component<{}, MyState> {
             <span className="text-red-500">S</span>
             <span className="text-yellow-500">a</span>
             <span className="text-blue-500">y</span>
-            </h1>
+          </h1>
           <input
             onChange={this.handleSearchChange.bind(this)}
             onKeyUp={this.handleEnterKeyPress.bind(this)}
@@ -115,20 +197,44 @@ class App extends React.Component<{}, MyState> {
           />
 
           <div className="flex">
-          <button
-            onClick={this.searchForTerm.bind(this)}
-            className="bg-blue-400 mt-2 hover:bg-blue-300 rounded text-white p-2 px-6 mr-2 focus:outline-none active:bg-blue-500"
-          >
-            <p className="font-semibold text-lg">Ask Gods</p>
-          </button>
-          <button
-            onClick={this.askXinecraft.bind(this)}
-            className="bg-gray-200 mt-2 hover:bg-gray-100 rounded text-gray-700 p-2 px-6 focus:outline-none active:bg-gray-300"
-          >
-            <p className="font-semibold text-lg">Ask Xinecraft</p>
-          </button>
-          </div>
+            <button
+              disabled={this.state.isLoading}
+              onClick={this.searchForTerm.bind(this)}
+              className="inline-flex items-center bg-blue-400 mt-2 hover:bg-blue-300 rounded text-white p-2 px-6 mr-2 focus:outline-none active:bg-blue-500 disabled:opacity-50"
+            >
+              {this.state.isLoading && (
+                <svg
+                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              )}
 
+              <span className="font-semibold text-lg">Ask Gods</span>
+            </button>
+            <button
+              disabled={this.state.isLoading}
+              onClick={this.askXinecraft.bind(this)}
+              className="inline-flex items-center bg-gray-200 mt-2 hover:bg-gray-100 rounded text-gray-700 p-2 px-6 focus:outline-none active:bg-gray-300 disabled:opacity-50"
+            >
+              <span className="font-semibold text-lg">Ask Xinecraft</span>
+            </button>
+          </div>
         </div>
 
         <div className="flex justify-around mt-5">
@@ -136,18 +242,18 @@ class App extends React.Component<{}, MyState> {
             <h3 className="text-center text-2xl text-blue-500 font-bhs mt-3">
               Bible {this.state.bibleResult?.totalCount}
             </h3>
-            <ul className="list-disc leading-5 p-7">
-              {bibleResult}
-            </ul>
+
+            {loadingBar}
+            {!this.state.isLoading && <ol className="list-disc leading-5 p-7 space-y-4">{bibleResult}</ol>}
           </div>
 
           <div className="h-full w-full border">
             <h3 className="text-center text-2xl text-green-500 font-bhs mt-3">
               Quran {this.state.quranResult?.totalCount}
             </h3>
-            <ul className="list-disc leading-5 p-7">
-              {quranResult}
-            </ul>
+
+            {loadingBar}
+            {!this.state.isLoading && <ol className="list-disc leading-5 p-7 space-y-4">{quranResult}</ol>}
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        'blue-gray': colors.blueGray,
+      }
+    },
   },
   variants: {
     extend: {
       backgroundColor: ['active'],
+      opacity: ['disabled'],
     },
   },
   plugins: [],
